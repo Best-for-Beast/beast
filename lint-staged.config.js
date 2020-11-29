fbcf021800e572b5b96e60b80d57ff098bfeb5e8
Szymon Graczyk <s.graczyk@livechatinc.com>
2020-11-29 19:09:56 +0100

    Test file change.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,10 @@ module.exports = {
   '*.{js,ts,svelte}': ['prettier --write', 'eslint --cache --fix'],
   '*.css': ['prettier --write'],
   '*.svelte': ['svelte-check'],
+  // 'docs/adr/*.md': () => [
+  //   'adr-log -d docs/adr -i',
+  //   'git add docs/adr/index.md',
+  // ],
   'docs/adr/*.md': () => [
     'adr-log -d docs/adr -i',
     'git add docs/adr/index.md',
